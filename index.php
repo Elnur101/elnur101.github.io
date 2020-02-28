f976220fe1dc07ace2e04b2d3194f0674b891068
Elnur101 <47320669+Elnur101@users.noreply.github.com>
2020-02-28 15:23:29 +0600

    index

---
+++ index.php
@@ -0,0 +1,363 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+    <link rel="stylesheet" href="web/css/main.css">
+    <link rel="stylesheet" href="web/fonts/css/font-awesome.min.css">
+
+    <title>Home</title>
+</head>
+<body>
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark fixed-top">
+    <div class="container">
+    <a class="navbar-brand" href="#">EBR</a>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul class="navbar-nav mr-auto">
+            <li class="nav-item active">
+                <a class="nav-link" href="index.php">Главная <span class="sr-only">(current)</span></a>
+            </li>
+            <li class="nav-item active">
+                <a class="nav-link" href="#"> О нас</a>
+            </li>
+            <li class="nav-item active">
+                <a class="nav-link"href="shop.php"> Магазин</a>
+            </li>
+            <li class="nav-item active">
+                <a class="nav-link" href=""> Контакты</a>
+            </li>
+        </ul>
+        <div class="login">
+            <a href="#"><i class="fa fa-youtube" aria-hidden="true"></i></a>
+            <a href="#"><i class="fa fa-instagram" aria-hidden="true"></i></a>
+            <a href="#"><i class="fa fa-user" aria-hidden="true"></i></a>
+            <button type="button" class="btn btn-primary"><i class="fa fa-download" aria-hidden="true">Download</i></button>
+
+        </div>
+    </div>
+    </div>
+</nav>
+
+<div id="carouselExampleCaptions" class="carousel slide" data-ride="carousel">
+    <ol class="carousel-indicators">
+        <li data-target="#carouselExampleCaptions" data-slide-to="0" class="active"></li>
+        <li data-target="#carouselExampleCaptions" data-slide-to="1"></li>
+        <li data-target="#carouselExampleCaptions" data-slide-to="2"></li>
+    </ol>
+    <div class="carousel-inner">
+        <div class="carousel-item active">
+            <img src="web/img/slide/1.jpg" class="d-block w-100" alt="...">
+            <div class="carousel-caption d-none d-md-block">
+                <h5>First slide label</h5>
+                <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+            </div>
+        </div>
+        <div class="carousel-item">
+            <img src="web/img/slide/1.jpg" class="d-block w-100" alt="...">
+            <div class="carousel-caption d-none d-md-block">
+                <h5>Second slide label</h5>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+            </div>
+        </div>
+        <div class="carousel-item">
+            <img src="web/img/slide/1.jpg" class="d-block w-100" alt="...">
+            <div class="carousel-caption d-none d-md-block">
+                <h5>Third slide label</h5>
+                <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
+            </div>
+        </div>
+    </div>
+    <a class="carousel-control-prev" href="#carouselExampleCaptions" role="button" data-slide="prev">
+        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+        <span class="sr-only">Previous</span>
+    </a>
+    <a class="carousel-control-next" href="#carouselExampleCaptions" role="button" data-slide="next">
+        <span class="carousel-control-next-icon" aria-hidden="true"></span>
+        <span class="sr-only">Next</span>
+    </a>
+</div>
+
+<div class="container">
+<div class="row">
+<div class="col-lg-4 col-md-4 col-sm-4 col-xs-12">
+               
+					<div class="box-part text-center">
+                        
+                        <img src="web/img/banner/3.jpg">
+                        
+						<div class="title">
+							<h4>Смартфоны</h4>
+						</div>
+                        
+						<div class="text">
+                        <a class="btn btn-outline-primary" href="#">Подробнее<i class="fa fa-chevron-right" aria-hidden="true"></i></a>
+						</div>
+                        
+					 </div>
+				</div>	 
+				
+				 <div class="col-lg-4 col-md-4 col-sm-4 col-xs-12">
+               
+					<div class="box-part text-center">
+					    
+                    <img src="web/img/banner/4.jpg">
+                    
+						<div class="title">
+							<h4>ПК и Комлектующие</h4>
+						</div>
+                        
+						<div class="text">
+                        <a class="btn btn-outline-primary" href="#">Подробнее<i class="fa fa-chevron-right" aria-hidden="true"></i></a>
+						</div>
+                        
+						
+                        
+					 </div>
+				</div>	 
+				
+				 <div class="col-lg-4 col-md-4 col-sm-4 col-xs-12">
+               
+					<div class="box-part text-center">
+                        
+                    <img src="web/img/banner/5.jpg">
+                        
+						<div class="title">
+							<h4>ТВ</h4>
+						</div>
+                        
+						<div class="text">
+                        <a class="btn btn-outline-primary" href="#">Подробнее<i class="fa fa-chevron-right" aria-hidden="true"></i></a>
+						</div>
+                        
+						
+                        
+					 </div>
+				</div>	 
+				
+				<div class="col-lg-4 col-md-4 col-sm-4 col-xs-12">
+               
+					<div class="box-part text-center">
+                        
+                        <i class="fa fa-pinterest-p fa-3x" aria-hidden="true"></i>
+                        
+						<div class="title">
+							<h4>Pinterest</h4>
+						</div>
+                        
+						<div class="text">
+							<span>Lorem ipsum dolor sit amet, id quo eruditi eloquentiam. Assum decore te sed. Elitr scripta ocurreret qui ad.</span>
+						</div>
+                        
+						<a class="btn btn-outline-primary" href="#">Подробнее<i class="fa fa-chevron-right" aria-hidden="true"></i></a>
+                        
+					 </div>
+				</div>	 
+				
+				 <div class="col-lg-4 col-md-4 col-sm-4 col-xs-12">
+               
+					<div class="box-part text-center">
+					    
+					    <i class="fa fa-google-plus fa-3x" aria-hidden="true"></i>
+                    
+						<div class="title">
+							<h4>Google</h4>
+						</div>
+                        
+						<div class="text">
+							<span>Lorem ipsum dolor sit amet, id quo eruditi eloquentiam. Assum decore te sed. Elitr scripta ocurreret qui ad.</span>
+						</div>
+                        
+						<a href="#">Learn More</a>
+                        
+					 </div>
+				</div>	 
+				
+				 <div class="col-lg-4 col-md-4 col-sm-4 col-xs-12">
+               
+					<div class="box-part text-center">
+                        
+                        <i class="fa fa-github fa-3x" aria-hidden="true"></i>
+                        
+						<div class="title">
+							<h4>Github</h4>
+						</div>
+                        
+						<div class="text">
+							<span>Lorem ipsum dolor sit amet, id quo eruditi eloquentiam. Assum decore te sed. Elitr scripta ocurreret qui ad.</span>
+						</div>
+                        
+						<a href="#">Learn More</a>
+                        
+					 </div>
+                </div>
+                
+		
+		</div>		
+</div>
+</div>
+
+<div class="container">
+    <div class="row">
+    <div id="inbox">
+          <div class="fab btn-group show-on-hover dropup">
+              <div data-toggle="tooltip" data-placement="left" title="Compose" style="margin-left: 42px;">
+          <button type="button" class="btn btn-danger btn-io dropdown-toggle" data-toggle="dropdown">
+            <span class="fa-stack fa-2x">
+                <i class="fa fa-circle fa-stack-2x fab-backdrop"></i>
+                <i class="fa fa-comments fa-stack-1x fa-inverse fab-primary"></i>
+                <i class="fa fa-pencil fa-stack-1x fa-inverse fab-secondary"></i>
+            </span>
+          </button></div>
+          <ul class="dropdown-menu dropdown-menu-right" role="menu">
+            <li><a href="#" data-toggle="tooltip" data-placement="left" title="Instagram"><i class="fa fa-instagram"></i></a></li>
+            <li><a href="#" data-toggle="tooltip" data-placement="left" title="Whatsapp"><i class="fa fa-whatsapp"></i></a></li>
+            <li><a href="#" data-toggle="tooltip" data-placement="left" title="Telegram"><i class="fa fa-telegram"></i></a></li>
+            <li><a href="#" data-toggle="tooltip" data-placement="left" title="Vkontakte"><i class="fa fa-vk"></i></a></li>
+          </ul>
+        </div>
+        </div>
+        <div class="title1">
+            <h4>Наши проекты</h4>
+        </div>
+        <div class="container p-5 ">
+            <div class="row">
+                <div class="col"> <div class="embed-responsive embed-responsive-16by9">
+                <iframe src="https://www.youtube.com/embed/EQNZVfGbaBU" class="embed-responsive-item" allowfullscreen></iframe>
+            </div>
+        </div>
+                <div class="col">
+                <div class="embed-responsive embed-responsive-16by9">
+                <iframe src="https://www.youtube.com/embed/EQNZVfGbaBU" class="embed-responsive-item" allowfullscreen></iframe>
+            </div>  
+                </div>
+                <div class="col">
+                <div class="embed-responsive embed-responsive-16by9">
+                <iframe src="https://www.youtube.com/embed/EQNZVfGbaBU" class="embed-responsive-item" allowfullscreen></iframe>
+            </div>  
+                </div>     
+                </div>
+        </div>
+    </div>
+    <div class="row">
+                <div class="col"> <div class="embed-responsive embed-responsive-16by9">
+                <iframe src="https://www.youtube.com/embed/EQNZVfGbaBU" class="embed-responsive-item" allowfullscreen></iframe>
+            </div>
+        </div>
+                <div class="col">
+                <div class="embed-responsive embed-responsive-16by9">
+                <iframe src="https://www.youtube.com/embed/EQNZVfGbaBU" class="embed-responsive-item" allowfullscreen></iframe>
+            </div>  
+                </div>
+                <div class="col">
+                <div class="embed-responsive embed-responsive-16by9">
+                <iframe src="https://www.youtube.com/embed/EQNZVfGbaBU" class="embed-responsive-item" allowfullscreen></iframe>
+            </div>  
+                </div>     
+                </div>
+        </div>
+    </div>
+</div>
+
+<!-- Footer -->
+<footer class="page-footer font-small dark pt-4">
+
+  <!-- Footer Links -->
+  <div class="container-fluid text-center text-md-left">
+
+    <!-- Grid row -->
+    <div class="row">
+
+      <!-- Grid column -->
+      <div class="col-md-6 mt-md-0 mt-3">
+
+        <!-- Content -->
+        <div class="container">
+        <h5 class="text-uppercase">EBR</h5>
+        <p>Here you can use rows and columns to organize your footer content.</p>
+                </div>
+      </div>
+      <!-- Grid column -->
+
+      <hr class="clearfix w-100 d-md-none pb-3">
+
+      <!-- Grid column -->
+      <div class="col-md-3 mb-md-0 mb-3">
+
+        <!-- Links -->
+        <h5 class="text-uppercase">Links</h5>
+
+        <ul class="list-unstyled">
+          <li>
+            <a href="#!">Главная</a>
+          </li>
+          <li>
+            <a href="#!">О нас</a>
+          </li>
+          <li>
+            <a href="#!">Магазин</a>
+          </li>
+          <li>
+            <a href="#!">Контакты</a>
+          </li>
+        </ul>
+
+      </div>
+      <!-- Grid column -->
+
+      <!-- Grid column -->
+      <div class="col-md-3 mb-md-0 mb-3">
+
+        <!-- Links -->
+        <h5 class="text-uppercase">Соц сети</h5>
+
+        <ul class="list-unstyled">
+          <li>
+            <a href="#!">Link 1</a>
+          </li>
+          <li>
+            <a href="#!">Link 2</a>
+          </li>
+          <li>
+            <a href="#!">Link 3</a>
+          </li>
+          <li>
+            <a href="#!">Link 4</a>
+          </li>
+        </ul>
+
+      </div>
+      <!-- Grid column -->
+
+    </div>
+    <!-- Grid row -->
+
+  </div>
+  <!-- Footer Links -->
+
+  <!-- Copyright -->
+  <div class="footer-copyright text-center py-3">© 2020 Copyright:
+    <a href="https://mdbootstrap.com/"> MDBootstrap.com</a>
+  </div>
+  <!-- Copyright -->
+
+</footer>
+<!-- Footer -->
+
+
+
+<!-- jQuery first, then Popper.js, then Bootstrap JS -->
+<script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
+<script src="web/js/carousel.js"></script>
+<script src="web/js/script.js"></script>
+<script src="web/js/chat.js"></script>
+</body>
+</html>
